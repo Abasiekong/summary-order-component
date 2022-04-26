1affb3db499618d6e73cd9e3960e09699effabdb
Wisdom Ekandem <wisdomekan02@gmail.com>
2022-04-26 20:06:38 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'pale-blue': '#e0e8ff',
+        'bright-blue': '#3829e0',
+        'very-pale-blue': '#f5f7ff',
+        'desaturated-blue': '#7280a7',
+        'dark-blue': '#1f2f56'
+      },
+    },
+  },
+  plugins: [],
+}
