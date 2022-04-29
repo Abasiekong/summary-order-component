d9a4bd5bfbac0ceac84c17c5051cd839b3e735c2
Wisdom Ekandem <wisdomekan02@gmail.com>
2022-04-29 22:00:32 +0100

    update file contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'desaturated-blue': '#7280a7',
         'dark-blue': '#1f2f56'
       },
+
+      width: {
+        '450': '28.125em',
+        '330': '20.625em',
+        '355': '22.1875em',
+      }
     },
   },
   plugins: [],
